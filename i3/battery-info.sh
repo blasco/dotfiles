307aa23ea63803c9774bc5e84bd9e58d4e30a5c7
WooWapDaBug <blascoburguillos@gmail.com>
2018-07-25 11:53:14 +0200

    Added battery icon

--- i3/battery-info.sh
+++ i3/battery-info.sh
@@ -2,5 +2,5 @@
 
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 01:00:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "$BATTINFO"
+    DISPLAY=:0.0 /usr/bin/notify-send -i notification-battery-low "Low battery" "$BATTINFO"
 fi
