a19700b58dba56dc689dabee8fcd5b155715c84e
WooWapDaBug <blascoburguillos@gmail.com>
2018-08-07 10:20:03 +0200

    updates

--- i3/battery-info.sh
+++ i3/battery-info.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 01:00:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send -i notification-battery-low "Low battery" "$BATTINFO"
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 01:15:00 ]] ; then
+    notify-send 'hi'
+    DISPLAY=:0.0 notify-send -i notification-battery-low "Low battery" "$BATTINFO"
 fi
